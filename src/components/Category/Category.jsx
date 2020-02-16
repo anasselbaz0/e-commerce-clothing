2f98d6fdac6dbe513bf354eaeb7fda539df24c01
Anass ELBAZ <anasselbaz0@gmail.com>
2020-02-16 13:58:58 +0100

    Add structure to Components

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './Category.css'
+import './Category.sass'
 
 export class Category extends Component {
 
--- src/components/ListCategory/ListCategories.jsx
+++ src/components/ListCategory/ListCategories.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import './ListCategories.sass';
 import Category from './Category'
 
 
